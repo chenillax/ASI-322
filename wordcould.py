5629d35d995ca7b8b0cb3f7bce4391898fbd745e
TGuerillon <tristan.guerillon@hotmail.com>
2022-11-28 10:24:02 +0100

    forme et rapprochement des mots

--- wordcould.py
+++ wordcould.py
@@ -3,17 +3,25 @@ import matplotlib.pyplot as plt
 from collections import ChainMap
 import pandas as pd
 import pyspark.sql.functions as F
+import numpy as np
 
 from schema import df
 
-#Compte les articles 
+# Compte les articles
 article = df.groupBy("Article").count().sort("count")
 
-#article.write.csv('mycsv.csv')
+# article.write.csv('mycsv.csv')
+size=800
+x, y = np.ogrid[:size, :size]
 
-wordcloud = WordCloud(width = 800, height = 400, background_color="white")
-words = dict(ChainMap(*article.select(F.create_map('Article', 'count')).rdd.map(lambda x: x[0]).collect()))
+mask = (x - (size/2)) ** 2 + (y - (size/2)) ** 2 > 400 ** 2
+mask = 255 * mask.astype(int)
+
+wordcloud = WordCloud(background_color="white", repeat=True,
+                      mask=mask,max_words=200)
+words = dict(ChainMap(*article.select(F.create_map('Article',
+             'count')).rdd.map(lambda x: x[0]).collect()))
 
 plt.imshow(wordcloud.generate_from_frequencies(words))
-
+plt.axis('off')
 plt.show()
