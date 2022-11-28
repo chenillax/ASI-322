fa9e2d510e4543d84e6f5ff5664fe36b9d4f3cf8
TGuerillon <tristan.guerillon@hotmail.com>
2022-11-28 10:46:13 +0100

    updated loaders

--- types.py
+++ types.py
@@ -2,18 +2,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType,DoubleType, FloatType,DateType
 spark = SparkSession. builder.master("local").appName("Import File").getOrCreate()
 
-data = "Bakery_sales.csv"
+data = "article_type.csv"
 
 schema = StructType()\
-    .add("",IntegerType(),True)\
-    .add("Date", DateType(), True)\
-    .add("Time",StringType(),True)\
-    .add("TicketNumber",FloatType(),True)\
     .add("Article",StringType(),True)\
     .add("Quantity",FloatType(),True)\
-    .add("UnitPrice",StringType(),True)\
+    .add("Type",StringType(),True)\
 
-df_sales= spark.read\
-    .option("header",True)\
+df_type= spark.read\
+    .option("header",False)\
     .schema(schema)\
     .csv(data)
