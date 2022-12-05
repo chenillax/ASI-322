4ceaa9b223717f27c6248788f4add3667f9a2d5b
TGuerillon <tristan.guerillon@hotmail.com>
2022-12-05 12:19:40 +0100

    fuck this shit i'm out

--- sales_in_time.py
+++ sales_in_time.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 import pyspark.sql.functions as F
 from pyspark.sql.types import StringType
 from pyspark.sql.functions import when, col, udf
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, FloatType, DateType
+from plotly.subplots import make_subplots
 
 
 def mult(quantity, price):
     return round(quantity*float(price.replace(',', '.')), 2)
 
 
-sales_quantity_by_type = df_sales.groupBy("Type").sum("Quantity").sort(F.col("sum(Quantity)").desc()).toPandas().plot.bar(x="Type", y="sum(Quantity)")
-multiply = udf(mult)
-df_sales.withColumn("subtotal", multiply("Quantity", "UnitPrice")).show()
+sales_quantity_by_type = df_sales.groupBy("Type").sum("Quantity").sort(F.col("sum(Quantity)").desc())#.toPandas().plot.bar(x="Type", y="sum(Quantity)")
+multiply = udf(mult, FloatType())
+sales_subtotal_by_type = df_sales.withColumn("subtotal", multiply("Quantity", "UnitPrice")).groupBy("Type").sum(
+    "subtotal").sort(F.col("sum(subtotal)").desc())#.toPandas().plot.bar(x="Type", y="sum(subtotal)")
+sales_quantity_by_type.join(sales_subtotal_by_type,sales_quantity_by_type.Type==sales_subtotal_by_type.Type,"fullouter").toPandas().plot.bar(stacked=True)
 plt.show()
--- schema.py
+++ schema.py
@@ -1,20 +1,20 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType,StructField, StringType, IntegerType,DoubleType, FloatType,DateType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, FloatType, DateType
 spark = SparkSession. builder.master("local").appName("Import File").getOrCreate()
 
 data = "Bakery_sales.csv"
 
 schema = StructType()\
-    .add("",IntegerType(),True)\
+    .add("", IntegerType(), True)\
     .add("Date", StringType(), True)\
-    .add("Time",StringType(),True)\
-    .add("TicketNumber",FloatType(),True)\
-    .add("Article",StringType(),True)\
-    .add("Quantity",FloatType(),True)\
-    .add("UnitPrice",StringType(),True)\
-    .add("Type",StringType(),True)\
+    .add("Time", StringType(), True)\
+    .add("TicketNumber", FloatType(), True)\
+    .add("Article", StringType(), True)\
+    .add("Quantity", FloatType(), True)\
+    .add("UnitPrice", StringType(), True)\
+    .add("Type", StringType(), True)\
 
-df_sales= spark.read\
-    .option("header",True)\
+df_sales = spark.read\
+    .option("header", True)\
     .schema(schema)\
     .csv(data)
