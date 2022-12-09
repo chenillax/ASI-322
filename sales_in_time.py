69733067c00e181f0ccd9acc970be7633f789e77
TGuerillon <tristan.guerillon@hotmail.com>
2022-12-09 14:37:34 +0100

    sales compare quantity over value

--- sales_in_time.py
+++ sales_in_time.py
@@ -12,9 +12,11 @@ def mult(quantity, price):
     return round(quantity*float(price.replace(',', '.')), 2)
 
 
-sales_quantity_by_type = df_sales.groupBy("Type").sum("Quantity").sort(F.col("sum(Quantity)").desc())#.toPandas().plot.bar(x="Type", y="sum(Quantity)")
+sales_quantity_by_type = df_sales.groupBy("Type").sum("Quantity").sort(F.col("sum(Quantity)").desc())  # .toPandas().plot.bar(x="Type", y="sum(Quantity)")
 multiply = udf(mult, FloatType())
 sales_subtotal_by_type = df_sales.withColumn("subtotal", multiply("Quantity", "UnitPrice")).groupBy("Type").sum(
-    "subtotal").sort(F.col("sum(subtotal)").desc())#.toPandas().plot.bar(x="Type", y="sum(subtotal)")
-sales_quantity_by_type.join(sales_subtotal_by_type,sales_quantity_by_type.Type==sales_subtotal_by_type.Type,"fullouter").toPandas().plot.bar(stacked=True)
+    "subtotal").sort(F.col("sum(subtotal)").desc())  # .toPandas().plot.bar(x="Type", y="sum(subtotal)")
+sales_quant_value = sales_quantity_by_type.join(sales_subtotal_by_type, sales_quantity_by_type.Type == sales_subtotal_by_type.Type, "inner").select(
+    [sales_quantity_by_type.Type, "sum(Quantity)", "sum(subtotal)"]).toPandas()
+sales_quant_value.plot.barh(x='Type',rot=0, title="Ventes: quantités par rapport au chiffre d'affaire par type")
 plt.show()
